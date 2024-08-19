4296a960ab356f51b520789cda463ac3106cb47b
Ayu <claire@ayu.land>
2024-08-19 17:03:17 +0800

    Toggle LED back light

---
+++ fw/debug/run.sh
@@ -0,0 +1,23 @@
+# ~/.platformio/packages/tool-openocd/bin/openocd -f interface/cmsis-dap.cfg -f target/stm32g0x.cfg -c 'adapter speed 32000'
+
+# (cd ~/Downloads/stlink; ./build/Release/bin/st-info --probe --connect-under-reset)
+# (cd ~/Downloads/stlink; ./build/Release/bin/st-flash --connect-under-reset write $OLDPWD/.pio/build/dev/firmware.bin 0x8000000)
+# ~/.platformio/packages/tool-openocd/bin/openocd -f interface/cmsis-dap.cfg -f target/stm32g0x.cfg -c 'program {.pio/build/dev/firmware.elf} verify reset; shutdown'
+
+cat >debug/gdbinit <<EOF
+define hook-quit
+  set confirm off
+end
+target extended-remote localhost:3333
+
+b swv_trap_line
+commands
+  silent
+  printf "%s\n", swv_buf
+  c
+end
+r
+EOF
+
+~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-gdb .pio/build/dev/firmware.elf -x debug/gdbinit
+rm debug/gdbinit
