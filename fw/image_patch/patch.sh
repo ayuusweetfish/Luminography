d4caf28d79ed8c687905cef772081cdc515bd9ba
Ayu <claire@ayu.land>
2025-08-03 17:55:13 +0800

    fw: Remove `image_patch/` script (no longer necessary)

--- fw/image_patch/patch.sh
+++
@@ -1,2 +0,0 @@
-cp /media/ayu/rootfs/home/ayu/f1c_build/images/flash.bin flash.bin
-cpp -I./include -traditional-cpp suniv-f1c100s-licheepi-nano.dts 2> /dev/null | dtc -I dts -O dtb /dev/stdin | dd if=/dev/stdin of=flash.bin bs=1K seek=512 count=8 conv=notrunc
