b00f744b0adefd2aa4d6c3882c996fbc440f1a31
Ayu <claire@ayu.land>
2025-08-04 17:05:11 +0800

    fw: Add SIGINT handler to blink

--- fw/rootfs_patch/blink.c
+++ fw/rootfs_patch/blink.c
@@ -1,12 +1,28 @@
 // /media/ayu/rootfs/home/ayu/f1c_build/host/bin/arm-linux-gcc blink.c -o rootfs/root/blink -O2
 
 // Reference: https://docs.kernel.org/userspace-api/gpio/chardev.html
+#include <assert.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>   // exit
+#include <signal.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/gpio.h>
+
+static int req_fd = 0;
+static void sigint_handler(int n)
+{
+  if (req_fd != 0) {
+    printf("Received SIGINT, turning off LED and exiting\n");
+    struct gpio_v2_line_values values = { .bits = 0, .mask = 1 };
+    if (ioctl(req_fd, GPIO_V2_LINE_SET_VALUES_IOCTL, &values) < 0) {
+      perror("GPIO_V2_LINE_SET_VALUES_IOCTL");
+    }
+    close(req_fd);
+  }
+  exit(0);
+}
 
 int main()
 {
@@ -31,15 +47,15 @@ int main()
 
   close(fd);
 
+  req_fd = req.fd;
+  signal(SIGINT, sigint_handler);
+
   printf("Blinking LED at GPIO line %d\n", line);
   int parity = 0;
   while (1) {
     parity ^= 1;
     puts(parity ? "On" : "Off");
-    struct gpio_v2_line_values values = {
-      .bits = parity,
-      .mask = 1,
-    };
+    struct gpio_v2_line_values values = { .bits = parity, .mask = 1 };
     if (ioctl(req.fd, GPIO_V2_LINE_SET_VALUES_IOCTL, &values) < 0) {
       perror("GPIO_V2_LINE_SET_VALUES_IOCTL");
       break;
@@ -47,7 +63,5 @@ int main()
     usleep(500000);
   }
 
-  close(req.fd);
-
-  return 0;
+  assert(0);
 }
